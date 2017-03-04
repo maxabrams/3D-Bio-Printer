6a8cc126c0898aa29d675f49b1e8900f85d2679b
Cynthia Le <Cynthia Le>
2017-03-04 10:47:32 -0800

    image capture rate input

--- Box_UI/src/Dish.java
+++ Box_UI/src/Dish.java
@@ -1,42 +1,57 @@
-
+import java.util.Date;
 
 public class Dish {
-	private int picsPerMinute;
+	private int captureRate;
+	private String captureMetric;
+
 	private String fileName;
-	//IDK if time should be String or INT
-	private int timeStart;
+	private Date timeStart;
+	private int lastPictureTaken;
 	private int picsTaken;
-	
-	//default picsPerMinute=1
-	public Dish(){
-		picsPerMinute=1;
-		fileName="";
-		timeStart=-1;
-		picsTaken=0;
+
+	// default picsPerMinute=1
+	public Dish() {
+		captureRate = 1;
+		fileName = "";
+		picsTaken = 0;
+		captureMetric= "mins";
 	}
-	
-	//every time take a picture, increment picsTaken count by 1
-	public void takePic(){
-		picsTaken+=1;
+
+	// every time take a picture, increment picsTaken count by 1
+	public void takePic() {
+		picsTaken += 1;
 	}
-	
-	public int getPicsPerMinute() {
-		return picsPerMinute;
+
+	public int getCaptureRate() {
+		return captureRate;
 	}
-	public void setPicsPerMinute(int picsPerMinute) {
-		this.picsPerMinute = picsPerMinute;
+
+	public void setCaptureRate(int captureRate) {
+		this.captureRate = captureRate;
 	}
+
 	public String getFileName() {
 		return fileName;
 	}
+
 	public void setFileName(String fileName) {
 		this.fileName = fileName;
 	}
-	public int getTimeStart() {
+
+	public String getCaptureMetric() {
+		return captureMetric;
+	}
+
+	public void setCaptureMetric(String captureMetric) {
+		this.captureMetric= captureMetric;
+	}
+
+	public Date getTimeStart() {
 		return timeStart;
 	}
-	public void setTimeStart(int timeStart) {
-		this.timeStart = timeStart;
+
+	public void setTimeStart(java.util.Date timeStamp) {
+		this.timeStart = timeStamp;
 	}
 
 	public int getPicsTaken() {
@@ -46,5 +61,5 @@ public class Dish {
 	public void setPicsTaken(int picsTaken) {
 		this.picsTaken = picsTaken;
 	}
-	
+
 }
